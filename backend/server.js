4b78974f1bc4aa4543195bc0ba4b9d40ee7aae4d
1ajthomas248 <1ajthomas248@gmail.com>
2025-08-02 07:05:27 -0500

    init backend ccess token calls and react environment

---
+++ backend/server.js
@@ -0,0 +1,245 @@
+// server init
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const util = require('util');
+const moment = require('moment');
+
+
+const APP_PORT = process.env.APP_PORT || 8000
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID
+const PLAID_SECRET = process.env.PLAID_SECRET
+const PLAID_ENV = process.env.PLAID_ENV
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(
+  ',',
+);
+
+// PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
+// will be able to select institutions from.
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+  ',',
+);
+
+// Parameters used for the OAuth redirect Link flow
+const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
+const PLAID_ANDROID_PACKAGE_NAME = process.env.PLAID_ANDROID_PACKAGE_NAME || '';
+
+
+let ACCESS_TOKEN = null;
+let USER_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+let ACCOUNT_ID = null;
+
+let PAYMENT_ID = null;
+
+let AUTHORIZATION_ID = null;
+let TRANSFER_ID = null;
+
+// Initialize the Plaid client
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
+      'PLAID-SECRET': PLAID_SECRET,
+      'Plaid-Version': '2020-09-14',
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/info', function (request, response, next) {
+  response.json({
+    item_id: ITEM_ID,
+    access_token: ACCESS_TOKEN,
+    products: PLAID_PRODUCTS,
+  });
+});
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side.
+
+app.post('/api/create_link_token', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const configs = {
+        user: {
+          // This should correspond to a unique id for the current user.
+          client_user_id: 'user-id',
+        },
+        client_name: 'Plaid Quickstart',
+        products: PLAID_PRODUCTS,
+        country_codes: PLAID_COUNTRY_CODES,
+        language: 'en',
+      };
+
+      if (PLAID_REDIRECT_URI !== '') {
+        configs.redirect_uri = PLAID_REDIRECT_URI;
+      }
+
+      if (PLAID_ANDROID_PACKAGE_NAME !== '') {
+        configs.android_package_name = PLAID_ANDROID_PACKAGE_NAME;
+      }
+      if (PLAID_PRODUCTS.includes(Products.Statements)) {
+        const statementConfig = {
+          end_date: moment().format('YYYY-MM-DD'),
+          start_date: moment().subtract(30, 'days').format('YYYY-MM-DD'),
+        }
+        configs.statements = statementConfig;
+      }
+
+      if (PLAID_PRODUCTS.some(product => product.startsWith("cra_"))) {
+        configs.user_token = USER_TOKEN;
+        configs.cra_options = {
+          days_requested: 60
+        };
+        configs.consumer_report_permissible_purpose = 'ACCOUNT_REVIEW_CREDIT';
+      }
+      const createTokenResponse = await client.linkTokenCreate(configs);
+      prettyPrintResponse(createTokenResponse);
+      response.json(createTokenResponse.data);
+    })
+    .catch(next);
+});
+
+// Create a user token which can be used for Plaid Check, Income, or Multi-Item link flows
+
+app.post('/api/create_user_token', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+
+      const request = {
+        // Typically this will be a user ID number from your application. 
+        client_user_id: 'user_' + uuidv4()
+      }
+
+      if (PLAID_PRODUCTS.some(product => product.startsWith("cra_"))) {
+        request.consumer_report_user_identity = {
+          date_of_birth: '1980-07-31',
+          first_name: 'Harry',
+          last_name: 'Potter',
+          phone_numbers: ['+16174567890'],
+          emails: ['harrypotter@example.com'],
+          primary_address: {
+            city: 'New York',
+            region: 'NY',
+            street: '4 Privet Drive',
+            postal_code: '11111',
+            country: 'US'
+          }
+        }
+      }
+      const user = await client.userCreate(request);
+      USER_TOKEN = user.data.user_token
+      response.json(user.data);
+    }).catch(next);
+});
+
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side
+app.post(
+  '/api/create_link_token_for_payment',
+  function (request, response, next) {
+    Promise.resolve()
+      .then(async function () {
+        const createRecipientResponse =
+          await client.paymentInitiationRecipientCreate({
+            name: 'Harry Potter',
+            iban: 'GB33BUKB20201555555555',
+            address: {
+              street: ['4 Privet Drive'],
+              city: 'Little Whinging',
+              postal_code: '11111',
+              country: 'GB',
+            },
+          });
+        const recipientId = createRecipientResponse.data.recipient_id;
+        prettyPrintResponse(createRecipientResponse);
+
+        const createPaymentResponse =
+          await client.paymentInitiationPaymentCreate({
+            recipient_id: recipientId,
+            reference: 'paymentRef',
+            amount: {
+              value: 1.23,
+              currency: 'GBP',
+            },
+          });
+        prettyPrintResponse(createPaymentResponse);
+        const paymentId = createPaymentResponse.data.payment_id;
+
+        // We store the payment_id in memory for demo purposes - in production, store it in a secure
+        // persistent data store along with the Payment metadata, such as userId.
+        PAYMENT_ID = paymentId;
+
+        const configs = {
+          client_name: 'Plaid Quickstart',
+          user: {
+            // This should correspond to a unique id for the current user.
+            // Typically, this will be a user ID number from your application.
+            // Personally identifiable information, such as an email address or phone number, should not be used here.
+            client_user_id: uuidv4(),
+          },
+          // Institutions from all listed countries will be shown.
+          country_codes: PLAID_COUNTRY_CODES,
+          language: 'en',
+          // The 'payment_initiation' product has to be the only element in the 'products' list.
+          products: [Products.PaymentInitiation],
+          payment_initiation: {
+            payment_id: paymentId,
+          },
+        };
+        if (PLAID_REDIRECT_URI !== '') {
+          configs.redirect_uri = PLAID_REDIRECT_URI;
+        }
+        const createTokenResponse = await client.linkTokenCreate(configs);
+        prettyPrintResponse(createTokenResponse);
+        response.json(createTokenResponse.data);
+      })
+      .catch(next);
+  },
+);
+
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+app.post('/api/set_access_token', function (request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  Promise.resolve()
+    .then(async function () {
+      const tokenResponse = await client.itemPublicTokenExchange({
+        public_token: PUBLIC_TOKEN,
+      });
+      prettyPrintResponse(tokenResponse);
+      ACCESS_TOKEN = tokenResponse.data.access_token;
+      ITEM_ID = tokenResponse.data.item_id;
+      response.json({
+        // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
+        access_token: ACCESS_TOKEN,
+        item_id: ITEM_ID,
+        error: null,
+      });
+    })
+    .catch(next);
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
